0735f2c5581e94541278545492018dc770453d02
Martinsito Brito Diaz <martinsitobd@gmail.com>
2022-01-16 15:05:05 -0400

    get method works with two superheroes

---
+++ SuperHeroAPI/Controllers/SuperHeroAPI.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroAPI : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero{
+                    id = 1,
+                    name = "Batman",
+                    firstName = "Bruce",
+                    lastName = "Wayne",
+                    place = "Gothan City",
+                    comic = "DC"
+                },                
+                new SuperHero{ 
+                    id = 2, 
+                    name = "Spiderman", 
+                    firstName = "Peter", 
+                    lastName = "Parked", 
+                    place = "New York City",
+                    comic = "Marvel"
+                 }
+            };
+
+            return Ok(heroes);
+        }
+    }
+}
---
+++ SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+        public string firstName { get; set; } = string.Empty;
+        public string lastName { get; set; } = string.Empty;
+        public string place { get; set; } = string.Empty;
+        public string comic { get; set; } = string.Empty;
+    }
+}
